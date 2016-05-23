fd9bc5aaa800fbea071c525c273118652bf5525b
an.sokolov <sokolik_85@list.ru>
2016-05-23 23:26:18 +0600

    inistansiated versoin 3 (index3 with table)

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(methodOverride());
 
 // routes ======================================================================
 app.get('*', function(req, res) {
-    res.sendFile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    res.sendFile('./public/index3.html'); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 
